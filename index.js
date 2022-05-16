50bb2a7f54300ed52214d80ed2e978f623f15ff6
Chendra07 <chendra-dewangga@emporiadigital.com>
2022-05-16 14:41:33 +0700

    edit index js again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3300;
+var port = process.env.PORT || 8080;
 const cors = require("cors");
 
 const routes = require("./routes");
