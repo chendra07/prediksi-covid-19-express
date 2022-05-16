0c01323d5d3b8055506a7702651040b1ae0fd79a
Chendra07 <chendra-dewangga@emporiadigital.com>
2022-05-16 14:30:02 +0700

    edit index js

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(express.json());
 
 app.use(routes);
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
